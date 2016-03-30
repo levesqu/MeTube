cfa2f85445eb932ad9f0f6e3f3382924c438d9d3
Christopher Levesque <levesqu@joey12.cs.clemson.edu>
2016-03-30 15:42:29 -0400

    bootstrap added to default.css and nav bar added to index.php

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <body>
+
+<!--the nav bar starts here -->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">METUBE</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- nav bar ends here -->
+ 
 <?php
 echo "<h1> Welcome to Metube! </h1>
 <h2>The place to share all your media.</h2>
 <h3>Log in or register below</h3> ";
 ?>
-
+<div class="container-fluid">
 <form action="login.php" method="post">
 	
 	<input type="submit" class="button"  VALUE = "Log in" >
@@ -16,6 +68,7 @@ echo "<h1> Welcome to Metube! </h1>
 	
 	<input type="submit" class="button"  VALUE = "Register" >
 </form>
+</div>
 
 </body>
 </html>
