53be12dcd8e018d4a3a15ef998396027c941627a
levesqu <levesqu@g.clemson.edu>
2016-04-12 21:28:42 -0400

    media upload changes

--- media.php
+++ media.php
@@ -6,6 +6,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/default.css" />
 <title>Media</title>
 <script src="Scripts/AC_ActiveX.js" type="text/javascript"></script>
 <script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
