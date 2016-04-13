14ff902e9c33054c5d98eb9a59034458243d8721
Nathan Coelho Jones <ncj@joey7.cs.clemson.edu>
2016-04-13 19:38:52 -0400

    minor comment changes

--- media.php
+++ media.php
@@ -158,10 +158,12 @@ if(isset($_GET['id'])) {
 			?>
 			<tr>
 				<td>
-					<form class="form-horizontal" method="post" action=""
-					<label class="col-lg-2 control-label"><?php$_SESSION['username']?>:</label>
-					<br>
-			
+					<form class="form-horizontal" method="post" action="">
+						<label class="col-lg-2 control-label"><?php echo $_SESSION['username']?>:</label>
+						<input type="textarea" name="userComment" />
+					</form>
+				</td>
+			</tr>
 		</table>
 	</fieldset>
 <?php
