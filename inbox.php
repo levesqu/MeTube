6afc78bff554b96f23444a4fff75d4e704ce9e59
levesqu <levesqu@g.clemson.edu>
2016-04-13 20:59:19 -0400

    inbox creation

--- inbox.php
+++ inbox.php
@@ -95,12 +95,11 @@ if (!$messages)
     </div>
 
 
-
     <div class="modal">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">blah blah&times;</button>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     <h4 class="modal-title">Modal title</h4>
                 </div>
                 <div class="modal-body">
