c963ef3935b2ae30e5297a38bee0f677a9ea7684
levesqu <levesqu@g.clemson.edu>
2016-04-13 21:14:24 -0400

    inbox creation

--- media.php
+++ media.php
@@ -64,16 +64,18 @@ if(isset($_GET['id'])) {
 	$type=$result_row[2];
 	if(substr($type,0,5)=="image") //view image
 	{
-		echo "Viewing Picture: ";
-		echo $result_row[5];
-        echo "<br><br>";
-		echo "<img src='".$filepath."'/>";
-
+	?>
+        <h3> "Viewing Picture: <?php echo $result_row[5];?>"</h3>
+        <br><br>
+		<img src='".$filepath."'/>";
+<?php
 	}
 	elseif (substr($type,0,5)=="video")//view movie
 	{	
 ?>
 	<h3>Viewing Video: <?php echo $result_row[5];?></h3>
+
+    <br><br>
 
         <object id="MediaPlayer" width=560 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
@@ -108,6 +110,8 @@ if(isset($_GET['id'])) {
         ?>
         <h3>Listening To Audio: <?php echo $result_row[5];?></h3>
 
+        <br><br>
+
         <object id="MediaPlayer" width=560 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
         <param name="filename" value="<?php echo $filepath?>">
         <param name="Showcontrols" value="True">
@@ -135,7 +139,7 @@ if(isset($_GET['id'])) {
 
 
      <h3>Other File: <?php echo $result_row[5];?></h3>
-     <br>
+     <br><br>
                 <tr class="success">
                     <td>
 
