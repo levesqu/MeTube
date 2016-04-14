2880df8b5e504fb0b4cf8771943ac67ec2d80cd5
levesqu <levesqu@g.clemson.edu>
2016-04-14 14:15:58 -0400

    editing messagethread

--- messageThread.php
+++ messageThread.php
@@ -62,7 +62,7 @@ $messagesender=$_SESSION['username'];
     echo "<p>$sendMessageTo</p>";
 
     // Comments start-->
-    $messagequery="select * from messages where messagereceiver=($messagesender OR $sendMessageTo) and messagesender=($messagesender OR $sendMessageTo) ";
+    $messagequery="select * from messages where messagereceiver=('$messagesender' OR '$sendMessageTo') and messagesender=('$messagesender' OR '$sendMessageTo'); ";
 
 
     $messages = mysql_query($messagequery);
