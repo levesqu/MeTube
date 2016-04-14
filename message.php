1737db7d352079e9035645f94e7dcc9b0b43fa9e
levesqu <levesqu@g.clemson.edu>
2016-04-13 22:32:25 -0400

    inbox creation

--- message.php
+++ message.php
@@ -6,40 +6,26 @@ include_once "function.php";
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
-    <title>Playlists - MeTube</title>
+    <title>Message - MeTube</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Playlists</title>
+    <title>New Message</title>
     <link rel="stylesheet" type="text/css" href="css/default.css" />
-    <script type="text/javascript" src="js/jquery-latest.pack.js"></script>
-    <script type="text/javascript">
-        function saveDownload(id)
-        {
-            $.post("media_download_process.php",
-                {
-                    id: id
-                },
-                function(message)
-                { }
-            );
-        }
-    </script>
-    <!--the nav bar starts here -->
+
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <div class="navbar-header">
+            <div class="navbpngar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                    <span clpngass="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="./browse.php">METUBE</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="./index.php">Home page <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Maybe fill with something?</a></li>
+                    <li class="active"><a href="./browse.php">Home page <span class="sr-only">(current)</span></a></li>
                 </ul>
                 <form class="navbar-form navbar-left" role="search">
                     <div class="form-group">
@@ -61,22 +47,55 @@ include_once "function.php";
 
 <body>
 
-<h2><?php echo $_SESSION['username'];?> Welcome To MeTube!</h2>
+<div class="addmargin">
+
+<h4 class="addmargin">New Message</h4><br>
 
 
-<h4 class="addmargin">Click one of the options below to browse media.</h4><br>
+<?php
 
-<div class="btn-group btn-group-justified">
-    <a href="./categories.php" class="btn btn-default">Categories</a>
-    <a href="./favorites.php" class="btn btn-default">Favorites</a>
-    <a href="./channels.php" class="btn btn-default">Channels</a>
-    <a href="./playlists.php" class="btn btn-default">Playlists</a>
-    <a href="./browse.php" class="btn btn-default">My Media</a>
-</div><br>
+$username = $_SESSION['username'];
+if (isset($_POST['categoryType']))
+{
+$category = $_POST['categoryType'];
 
+$query = "SELECT * from media WHERE mediaCategory='$category'";
 
-<br>
-<p>Here they will be able to select a playlist</p>
+$result = mysql_query( $query );
+if (!$result){
+    die ("Could not query the media table in the database: <br />". mysql_error());
+}
+?>
+<table class="table table-hover">
+    <?php
+    while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path, mediaTitle, mediaDescription, mediaTags, mediaCategory
+    {
+        $mediaid = $result_row[3];
+        $filename = $result_row[0];
+        $filepath = $result_row[4];
+        ?>
+
+        <tr class="success">
+            <td>
+
+                <?php
+                //echo $mediaid;  //mediaid
+                ?>
+            </td>
+            <td>
+                <a href="media.php?id=<?php echo $mediaid;?>" target="_blank">&nbsp;<?php echo $filename;?></a>
+            </td>
+            <td>
+                <a href="<?php echo $filepath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
+            </td>
+        </tr>
+        <?php
+    }
+    }
+    ?>
+    <br>
+</table>
+    </div>
 
 </body>
 </html>
