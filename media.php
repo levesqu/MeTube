cd42cd568d79bebba12dbb15fac7fc6c5813faae
levesqu <levesqu@g.clemson.edu>
2016-04-14 10:16:21 -0400

    adding add to play list button

--- media.php
+++ media.php
@@ -157,6 +157,7 @@ if(isset($_GET['id'])) {
      <br><br>
      <?php
 
+ }
 
 	$mediaId = $_GET['id'];
 	$username = $_SESSION['username'];
@@ -185,31 +186,15 @@ if(isset($_GET['id'])) {
          } ?>
          </div>
 
-<!--         <div class="btn-group">-->
-<!--             <a href="" class="btn btn-default">Default</a>-->
-<!--             <a href="" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>-->
-<!--             <ul class="dropdown-menu">-->
-<!--                 <li><a href="">Action</a></li>-->
-<!--                 <li><a href="">Another action</a></li>-->
-<!--                 <li><a href="">Something else here</a></li>-->
-<!--             </ul>-->
-<!--         </div>-->
-
-    <div class="btn-group">
-        <a href="#" class="btn btn-default">8</a>
-        <div class="btn-group">
-            <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                Dropdown
-                <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="#">Dropdown link</a></li>
-                <li><a href="#">Dropdown link</a></li>
-                <li><a href="#">Dropdown link</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
+         <div class="btn-group">
+             <a href="" class="btn btn-default">Default</a>
+             <a href="" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+             <ul class="dropdown-menu">
+                 <li><a href="">Action</a></li>
+                 <li><a href="">Another action</a></li>
+                 <li><a href="">Something else here</a></li>
+             </ul>
+         </div>
 
 
     <?php
