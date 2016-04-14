66222dcf9680c8ff0536c0186fd3ac8e786f2030
levesqu <levesqu@g.clemson.edu>
2016-04-13 22:19:23 -0400

    inbox creation

--- inbox.php
+++ inbox.php
@@ -70,7 +70,7 @@ if (!$messages)
         $messageSubject = $singleMessage[1];
         ?>
         <tr>
-            <td><?php echo $messageSender?>: &#09; <a href="#"><?php echo $messageSubject?> </a>
+            <td><?php echo $messageSender?>: &#09; <a href=""><?php echo $messageSubject?> </a>
         </tr>
         <?php
     }
@@ -79,26 +79,7 @@ if (!$messages)
 </table>
 </fieldset>
 
-
-    <a href="" class="btn btn-primary" data-toggle="modal" data-target="#modal">Click to open New Message</a>
-
-    <div class="modal fade" id="modal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Modal title</h4>
-                </div>
-                <div class="modal-body">
-                    <p>One fine body…</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <a href="" class="btn btn-primary" >New Message</a>
 
 
 </div>
