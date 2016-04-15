8546c3ce252b0ec8ea6a2572d8fb2c1e39b47045
Zachary S Lemieux <zlemieu@imp12.cs.clemson.edu>
2016-04-14 20:51:44 -0400

    improvements to profile update

--- profile_update.php
+++ profile_update.php
@@ -1,9 +1,26 @@
 <?php
+include "mysqlClass.inc.php";
 session_start();
 
+    //$query = "tester";
 
-    $query = "test";
+    //instead of blah i need to give it real username
+    $queryAge = "select age from account where username like 'Schafer';";
+    $queryWorkPlace = "select workplace from account where username like 'Schafer';";
+
+    $age = mysql_query($queryAge);
+    $workplace = mysql_query($queryWorkPlace);
+
+    $agerow = mysql_fetch_array($age);
+    $workplacerow = mysql_fetch_array($workplace);
     //$queryresult = mysql_query($query);
+
+
+    /*$eventid = $_GET['id'];
+    $field = $_GET['field'];
+    $result = mysql_query("SELECT $field FROM `events` WHERE `id` = '$eventid' ");
+    $row = mysql_fetch_array($result);
+    echo $row[$field];*/
 
 
 ?>
@@ -68,21 +85,21 @@ session_start();
         <div class="form-group">
             <label for="inputPassword" class="col-lg-2 control-label">Update Password:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputPassword" type="password" name="password" value="help")>
+                <input class="form-control" id="inputPassword" type="password" name="password" value="")>
             </div>
         </div>
 
         <div class="form-group">
             <label for="inputAge" class="col-lg-2 control-label">Update Age:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputAge" type="text" name="age" value="i dont really know">
+                <input class="form-control" id="inputAge" type="text" name="age" value="<?php echo $agerow[0];?>">
             </div>
         </div>
 
         <div class="form-group">
             <label for="inputWorkplace" class="col-lg-2 control-label">Update Place of Work:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputWorkplace" type="text" name="workplace" value="workname i dont really know">
+                <input class="form-control" id="inputWorkplace" type="text" name="workplace" value="<?php echo $workplacerow[0];?>">
             </div>
         </div>
 
