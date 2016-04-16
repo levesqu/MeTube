a68ea7e8b7b6ee7cc618dbac6bd9de65a39a12ad
levesqu <levesqu@g.clemson.edu>
2016-04-16 18:51:33 -0400

    editing messagethread

--- profile_update.php
+++ profile_update.php
@@ -5,26 +5,17 @@ session_start();
     //$query = "tester";
     $username=$_SESSION['username'];
 
-    //instead of blah i need to give it real username
-    $queryPassword = "select workplace from account where username='$username'";
-    $queryAge = "select age from account where username='$username'";
-    $queryWorkPlace = "select workplace from account where username='$username'";
+    $profile = mysql($queryProfile);
 
-    $password = mysql_query($queryPassword);
-    $age = mysql_query($queryAge);
-    $workplace = mysql_query($queryWorkPlace);
+    $profileInfo=mysql_fetch_row($profile);
 
-    $passwordrow = mysql_fetch_array($password);
-    $agerow = mysql_fetch_array($age);
-    $workplacerow = mysql_fetch_array($workplace);
-    //$queryresult = mysql_query($query);
+    $password = $profileInfo[1];
+    $age = $profileInfo[2];
+    $workplace = $profileInfo[3];
 
 
-    /*$eventid = $_GET['id'];
-    $field = $_GET['field'];
-    $result = mysql_query("SELECT $field FROM `events` WHERE `id` = '$eventid' ");
-    $row = mysql_fetch_array($result);
-    echo $row[$field];*/
+// things to add
+// first and last name, about me and confirm password.
 
 
 ?>
@@ -46,7 +37,6 @@ session_start();
 <form class="form-horizontal" method="post"  action="profile_update_process.php" enctype="multipart/form-data" >
     <fieldset>
         <legend>Update Profile</legend>
-        <!-- in here there was a second <form> tag and the top form tag at media upload procress in it not sure if that's what was meant to be but I changed it-->
 	<table width="100%">
         <div class="form-group">
 			<label for="inputUsername" class="col-lg-2 control-label">Update Username:</label>
@@ -58,21 +48,21 @@ session_start();
         <div class="form-group">
             <label for="inputPassword" class="col-lg-2 control-label">Update Password:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputPassword" type="password" name="password" value="<?php echo $passwordrow[0];?>")>
+                <input class="form-control" id="inputPassword" type="password" name="password" value="<?php echo $password;?>")>
             </div>
         </div>
 
         <div class="form-group">
             <label for="inputAge" class="col-lg-2 control-label">Update Age:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputAge" type="text" name="age" value="<?php echo $agerow[0];?>">
+                <input class="form-control" id="inputAge" type="text" name="age" value="<?php echo $age;?>">
             </div>
         </div>
 
         <div class="form-group">
             <label for="inputWorkplace" class="col-lg-2 control-label">Update Place of Work:</label>
             <div class="col-lg-10">
-                <input class="form-control" id="inputWorkplace" type="text" name="workplace" value="<?php echo $workplacerow[0];?>">
+                <input class="form-control" id="inputWorkplace" type="text" name="workplace" value="<?php echo $workplace;?>">
             </div>
         </div>
 
