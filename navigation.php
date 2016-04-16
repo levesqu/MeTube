4285f9b7c8d3d581a48640f9261a9acaec981a82
Nathan Coelho Jones <ncj@joey7.cs.clemson.edu>
2016-04-15 22:10:56 -0400

    separated logged in and logged out nav bars, currently must be logged in to do anything since not being logged in breaks things

--- navigation.php
+++ navigation.php
@@ -7,7 +7,40 @@
 		$message_count = mysql_num_rows($message_results);
 		$logged_in = 1;
 	}
-?>
+
+if (isset($logged_in)) { ?>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="browse.php">METUBE</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="browse.php">Home page <span class="sr-only">(current)</span></a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="inbox.php">Messages <span class="badge"><?php echo $message_count; ?></span>
+				<li><a href="profile_update.php">Update Profile</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<?php } else { ?>
 
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
@@ -18,30 +51,18 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="./browse.php">METUBE</a>
+			<a class="navbar-brand" href="browse.php">METUBE</a>
 		</div>
 
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="./browse.php">Home page <span class="sr-only">(current)</span></a></li>
+				<li class="active"><a href="index.php">Home page <span class="sr-only">(current)</span></a></li>
 			</ul>
-			<form class="navbar-form navbar-left" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
-				</div>
-				<button type="submit" class="btn btn-default">Submit</button>
-			</form>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="./inbox.php">Messages 
-					<?php if (isset($logged_in)) {?><span class="badge"><?php echo $message_count; ?></span><?php }?>
-					</a></li>
-				<li><a href="./profile_update.php">Update Profile</a></li>
-				<?php if (isset($logged_in)) { ?>
-					<li><a href="logout.php">Logout</a></li>
-				<?php } else { ?>
-					<li><a href="index.php">Login</a></li>
-				<?php } ?>
+				<li><a href="profile_update.php">Register</a></li>
+				<li><a href="login.php">Login</a></li>
 			</ul>
 		</div>
 	</div>
 </nav>
+<?php } ?>
