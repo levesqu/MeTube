22aa148cad4de687b6d95e185cea3a5ff4e9fc61
Nathan Coelho Jones <ncj@joey7.cs.clemson.edu>
2016-04-16 17:35:39 -0400

    fixed nav error

--- navigation.php
+++ navigation.php
@@ -1,4 +1,8 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include_once "function.php";
 	if (isset($_SESSION['username']))
 	{
 		$username = $_SESSION['username'];
