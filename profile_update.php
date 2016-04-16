0ee4cbadee30631df23c51c732aed98d452ff726
levesqu <levesqu@g.clemson.edu>
2016-04-16 18:53:47 -0400

    editing messagethread

--- profile_update.php
+++ profile_update.php
@@ -5,18 +5,15 @@ session_start();
     //$query = "tester";
     $username=$_SESSION['username'];
 
+    $queryProfile = "select * from account where username ='$username'";
     $profile = mysql($queryProfile);
 
     $profileInfo=mysql_fetch_row($profile);
 
+
     $password = $profileInfo[1];
     $age = $profileInfo[2];
     $workplace = $profileInfo[3];
-
-
-// things to add
-// first and last name, about me and confirm password.
-
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -37,6 +34,7 @@ session_start();
 <form class="form-horizontal" method="post"  action="profile_update_process.php" enctype="multipart/form-data" >
     <fieldset>
         <legend>Update Profile</legend>
+        <!-- in here there was a second <form> tag and the top form tag at media upload procress in it not sure if that's what was meant to be but I changed it-->
 	<table width="100%">
         <div class="form-group">
 			<label for="inputUsername" class="col-lg-2 control-label">Update Username:</label>
@@ -66,25 +64,6 @@ session_start();
             </div>
         </div>
 
-<!--
-        <tr>
-			<td  width="20%"><label for="inputPassword" class="col-lg-2 control-label">Update Password:</label></td>
-			<td width="80%"><input class="text"  type="password" name="password"><br /></td>
-		</tr>
-		<br>-->
-
-		<!-- label and input values need to be changed   -->
-		<!--<tr>
-			<td  width="20%"><label for="inputAge" class="col-lg-2 control-label">Update Age:</label></td>
-			<td width="80%"><input class="text"  type="text" name="age"><br /></td>
-		</tr>
-		<br>-->
-
-		<!-- label and input values need to be changed   -->
-		<!--<tr>
-			<td  width="20%"><label for="input_place_of_work" class="col-lg-2 control-label">Update Place of Work:</label></td>
-			<td width="80%"><input class="text"  type="text" name="workplace"><br /></td>
-		</tr>-->
 
         <div class="col-lg-10 col-lg-offset-2">
 			<input name="submit" type="submit" class="btn btn-primary" value="Update">
