efe575dd04209ee53326fddcbf12c9eb7f2fa73b
levesqu <levesqu@g.clemson.edu>
2016-04-19 09:52:52 -0400

    porfile to show that users media

--- profile.php
+++ profile.php
@@ -109,9 +109,47 @@ else {
         <?php  echo $lastname; ?></h3>
     <h4>About:</h4>
     <p> <?php echo $aboutme ?>  </p>
-    <?php
+
+<?php
 
 }
+
+$query = "SELECT * from media WHERE username='$username'";
+
+$result = mysql_query( $query );
+if (!$result){
+    die ("Could not query the media table in the database: <br />". mysql_error());
+}
+?>
+    <div style="background:#95a5a6;color:#FFFFFF; width:100%; margin:auto; text-align:center; padding-top: 10px; padding-bottom: 10px;"> My Media</div>
+    <table class="table table-hover">
+        <?php
+        while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path
+        {
+            $mediaid = $result_row[3];
+            $filename = $result_row[0];
+            $filepath = $result_row[4];
+            $title = $result_row[5];
+            ?>
+
+            <tr class="success">
+                <td />
+                <td>
+                    <a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $title;?></a>
+                </td>
+                <td>
+                    <a href="<?php echo $filepath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+        <br>
+    </table>
+
+
+
+
 ?>
 
 </div>
